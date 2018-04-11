53c5c48c3bb1751eacb0cfbf838411fece2017c4
pengshanglin <pengshanglin@sobey.com>
2018-04-11 10:50:02 +0800

    IP

--- reception/static/json/HTTPNAME.js
+++ reception/static/json/HTTPNAME.js
@@ -1,8 +1,8 @@
 var HTTP = {
-    url: "http://172.16.145.51:8088/mhq-mserver/"
+    url: "http://172.16.146.3:8088/mhq-mserver/"
     // url: "http://172.16.146.4:8068/mhq-mserver/"
 };
 var WS = {
-    url: "ws://172.16.145.51:8088/mhq-mserver/"
+    url: "ws://172.16.146.3:8088/mhq-mserver/"
     // url: "ws://172.16.146.4:8068/mhq-mserver/"
 };
